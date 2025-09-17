e9e1a4de34f9f0ca32d66a21d0bc3f5a83c6dec6
Ayelen Ibañez <ibanezayelen307@gmail.com>
2025-09-17 12:23:12 -0300

    creacion clase persona

---
+++ Proyecto persona/src/Persoma.java
@@ -0,0 +1,13 @@
+public class Persoma {
+    private int edad;
+    private String nombre;
+
+    public Persoma(int edad, String nombre) {
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+}
